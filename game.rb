4b8dab6d73e3f8f6075aaed5dc07af3422ea634e
headlulu888 <headlulu888@gmail.com>
2017-05-24 23:02:55 +0300

    added bukva

--- game.rb
+++ game.rb
@@ -23,8 +23,36 @@ class Game
 
   # Основной метод игры "сделать следующий шаг". В качестве параметра принимает
   # букву, которую ввел пользователь.
+
+
+  # def next_step(bukva)
+  #   if status == -1 || status == 1
+  #     return
+  #   end
+  #
+  #   if @good_letters.include?(bukva) || @bad_letters.include?(bukva)
+  #     return
+  #   end
+  #
+  #   if @letters.include?(bukva)
+  #     @good_letters << bukva
+  #
+  #     if @good_letters.uniq.sort == @letters.uniq.sort
+  #       @status = 1
+  #     end
+  #   else
+  #     @bad_letters << bukva
+  #     @errors += 1
+  #
+  #     if @errors >= 7
+  #       @status = -1
+  #     end
+  #   end
+  # end
+
   def next_step(bukva)
-    if status == -1 || status == 1
+
+    if @status == -1 || @status == 1
       return
     end
 
@@ -32,16 +60,57 @@ class Game
       return
     end
 
-    if @letters.include?(bukva)
+    if @letters.include?(bukva) ||
+        (bukva == "е" && letters.include?("ё")) ||
+        (bukva == "ё" && letters.include?("е")) ||
+        (bukva == "и" && letters.include?("й")) ||
+        (bukva == "й" && letters.include?("и"))
+
+      # Если в слове есть буква запишем её в число "правильных" буква
       @good_letters << bukva
 
-      if @good_letters.uniq.sort == @letters.uniq.sort
+      if bukva == "е"
+        good_letters << "ё"
+      end
+
+      if bukva == "ё"
+        good_letters << "е"
+      end
+
+      if bukva == "и"
+        good_letters << "й"
+      end
+
+      if bukva == "й"
+        good_letters << "и"
+      end
+      
+      if (letters - good_letters).empty?
         @status = 1
       end
     else
+
       @bad_letters << bukva
+
+      if bukva == "е"
+        bad_letters << "ё"
+      end
+
+      if bukva == "ё"
+        bad_letters << "е"
+      end
+
+      if bukva == "и"
+        bad_letters << "й"
+      end
+
+      if bukva == "й"
+        bad_letters << "и"
+      end
+
       @errors += 1
 
+      # Если ошибок больше 7 — статус игры меняем на -1, проигрыш.
       if @errors >= 7
         @status = -1
       end
